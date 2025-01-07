82ed41ab7590d8e84eb18cb42570685cfbf14de5
Gyeongseob <syhbong9@gmail.com>
2025-01-07 17:46:06 +0900

    Title: 중앙값 구하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250107/중앙값 구하기/find-the-median.py
+++ 250107/중앙값 구하기/find-the-median.py
@@ -4,8 +4,9 @@ li.append(int(inp[0]))
 li.append(int(inp[1]))
 li.append(int(inp[2]))
 
-for i in range(0, len(li)-1):
-    if li[i] > li[i+1]:
-        li[i],li[i+1] = li[i+1],li[i]
+for j in range(len(li)-1, 0, -1):
+    for i in range(j):
+        if li[i] > li[i+1]:
+            li[i],li[i+1] = li[i+1],li[i]
 
 print(li[1])
